aaca921b5ddc76d77a4824d06039322d3eea93fd
NormaBuendia <normabdv@gmail.com>
2024-06-22 16:57:56 -0400

    se cambio a testDESAFIO2

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement test2
+    # TODO implement test DESAFIO 2
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
